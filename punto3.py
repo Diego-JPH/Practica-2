ba9ae7702ffded413acc0605065d0deff4fd3f58
Diego <diegoph410@gmail.com>
2024-04-07 19:57:21 -0300

    Tercer punto de la practica

---
+++ punto3.py
@@ -0,0 +1,17 @@
+import string
+jupyter_info = """ JupyterLab is a web-based interactive development
+environment for Jupyter notebooks, code, and data. JupyterLab is
+flexible: configure and arrange the user interface to support a wide
+range of workflows in data science, scientific computing, and machine
+learning. JupyterLab is extensible and modular: write plugins that add
+new components and integrate with existing ones. """
+letra = str(input("Ingrese la letra: ")).lower()
+if (letra in string.ascii_lowercase or letra in string.ascii_uppercase):
+    for elem in jupyter_info.split():
+        palabra = []
+        for char in elem.lower():
+            palabra.append(char)
+        if (letra in palabra):
+            print (elem)
+else:
+    print ("El caracter ingresado no es una letra...")
